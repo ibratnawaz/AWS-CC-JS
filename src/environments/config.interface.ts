0c65a84c63d387dfa92ec30e4c3b6a3b0a217e00
Ibrat Nawaz <nawazmd0096@gmail.com>
2022-10-09 15:53:23 +0530

    Integrated the product list api

--- src/environments/config.interface.ts
+++ src/environments/config.interface.ts
@@ -1,7 +1,13 @@
-export type ApiEndpoint = 'product' | 'order' | 'import' | 'bff' | 'cart';
+export type ApiEndpoint =
+  | 'products'
+  | 'product'
+  | 'order'
+  | 'import'
+  | 'bff'
+  | 'cart'
 
 export interface Config {
-  production: boolean;
-  apiEndpoints: Record<ApiEndpoint, string>;
-  apiEndpointsEnabled: Record<ApiEndpoint, boolean>;
+  production: boolean
+  apiEndpoints: Record<ApiEndpoint, string>
+  apiEndpointsEnabled: Record<ApiEndpoint, boolean>
 }
